4616ae855442391639efaa1a6526c8a6ccc56a24
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2021-05-21 13:07:02 -0500

    Create visual studio project

---
+++ Task-Managment-System/Task-Managment-System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Task_Managment_System.Startup))]
+namespace Task_Managment_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
