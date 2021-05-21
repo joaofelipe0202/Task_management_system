a40a18ec99f84f6be83375b4b5b7bd2b42b5cb0f
Maggie <cdalke4@msn.com>
2021-05-21 18:16:15 -0500

    AddingProjectHelperFeatureAndUpdateProjectConstructor

--- Task-Managment-System/Task-Managment-System/Models/ProjectHelper.cs
+++ Task-Managment-System/Task-Managment-System/Models/ProjectHelper.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Task_Managment_System.Models
 {
     public class ProjectHelper
     {
+        static ApplicationDbContext db = new ApplicationDbContext();
+
+        [Authorize]
+        public static void Add(string name, double budget, DateTime deadline, string creatorId)
+        {
+            var newProject = new Project(name, budget, deadline, creatorId);
+            db.Projects.Add(newProject);
+        }
+
+        [Authorize]
+        public static void Delete(int projectId)
+        {
+            var project = db.Projects.Find(projectId);          
+            db.Projects.Remove(project);
+        }
+
+        [Authorize]
+        public static void Update(int projectId)
+        {
+            var project = db.Projects.Find(projectId);
+            db.Entry(project).State = EntityState.Modified;
+            db.SaveChanges();
+        }
     }
 }
