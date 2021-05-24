f942356b11d14c478f8144e7a7aa32530cf51e71
ltlkong <ltl2298475598@icloud.com>
2021-05-24 12:50:20 -0500

    seed project

--- Task-Managment-System/Task-Managment-System/Migrations/Configuration.cs
+++ Task-Managment-System/Task-Managment-System/Migrations/Configuration.cs
@@ -65,16 +65,27 @@
             //seed user ref: https://stackoverflow.com/questions/19280527/mvc-5-seed-users-and-roles
             SeedUser(context, "ltl@mw.com", "123456Ltl.", 130, "Developer");
 
-            SeedUser(context, "Jonny@mw.com", "123456Mw.", 130, "ProjectManager");      
+            var user = SeedUser(context, "Jonny@mw.com", "123456Mw.", 130, "ProjectManager");      
 
             SeedUser(context, "Adam@mw.com", "123456Mw.", 130, "Developer");
 
             SeedUser(context, "Courtney@mw.com", "123456Mw.", 130, "Developer");
 
             SeedUser(context, "Amanda@mw.com", "123456Mw.", 130, "Developer");
+
+            Project project = new Project("task management", 1300, DateTime.Now, user.Id);
+
+            ProjectTask task = new ProjectTask()
+            {
+                ManagerId = user.Id,
+                Title = "create git repo",
+                Contents = "lalal",
+                DateCreated = DateTime.Now,
+                Complete = false
+            };
         }
 
-        private void SeedUser(ApplicationDbContext context, string email, string password, float salary, string role)
+        private ApplicationUser SeedUser(ApplicationDbContext context, string email, string password, float salary, string role)
         {
             //UserName and Email have to be identical for now otherwise it will not work. we can fix this problem later.
             if (!context.Users.Any(u => u.UserName == email))
@@ -87,7 +98,7 @@
                     UserName = email,
                     Email = email,
                     DateCreated = DateTime.Now,
-                    DailySalaray = 130,
+                    DailySalaray = salary,
                     EmailConfirmed = false,
                     PhoneNumberConfirmed = false,
                     TwoFactorEnabled = true,
@@ -99,7 +110,11 @@
 
                 if(role != null)
                     manager.AddToRole(user.Id, role);
+
+                return user;
             }
+
+            return context.Users.FirstOrDefault(user => user.UserName == email);
         }
     }
 }
