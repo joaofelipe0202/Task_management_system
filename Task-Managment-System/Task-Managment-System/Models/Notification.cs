d5b63496e94661a133dc0692cc8ed1665d6995df
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2021-05-24 17:16:15 -0500

    Add Notifications
    Add Notification class
    Add NotificationType enumerated type
    Add Relations to Notification

---
+++ Task-Managment-System/Task-Managment-System/Models/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task_Managment_System.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Contents { get; set; }
+        public bool Read { get; set; }
+        public NotificationType NotificationType { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
