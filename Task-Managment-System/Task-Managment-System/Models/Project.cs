3ccd10e4b38ab118ec9047c30e468a10be1eb3ee
ltlkong <ltl2298475598@icloud.com>
2021-05-24 15:09:23 -0500

    some more data

--- Task-Managment-System/Task-Managment-System/Models/Project.cs
+++ Task-Managment-System/Task-Managment-System/Models/Project.cs
@@ -13,8 +13,13 @@ namespace Task_Managment_System.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public double Budget { get; set; }
+        [Required]
         public DateTime DateCreated { get; set; }
         public DateTime Deadline { get; set; }
+        public int Percentage { get; set; }
+        public double ActualCost { get; set; }
+        [Required]
+        public bool Complete { get; set; }
         public ApplicationUser Creator { get; set; }
         public ICollection<ApplicationUser> Members { get; set; }
         public ICollection<ProjectTask> Tasks { get; set; }
