d674fa24cb830e59b9f889e1bc3c4014303c785f
Adam Isaak <67387234+Adam-Isaak-dev@users.noreply.github.com>
2021-05-25 09:17:38 -0500

    Revert "Update Main with new Seed Data"

--- Task-Managment-System/Task-Managment-System/Models/Project.cs
+++ Task-Managment-System/Task-Managment-System/Models/Project.cs
@@ -11,23 +11,13 @@ namespace Task_Managment_System.Models
         public int Id { get; set; }
         public string CreatorId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public double Budget { get; set; }
-        [Required]
         public DateTime DateCreated { get; set; }
         public DateTime Deadline { get; set; }
-        public int Percentage { get; set; }
-        public double ActualCost { get; set; }
-        [Required]
-        public bool Complete { get; set; }
+
         public ApplicationUser Creator { get; set; }
         public ICollection<ApplicationUser> Members { get; set; }
         public ICollection<ProjectTask> Tasks { get; set; }
-
-        public Project()
-        {
-
-        }
 
         public Project(string name, double budget, DateTime deadline, string creatorId)
         {
