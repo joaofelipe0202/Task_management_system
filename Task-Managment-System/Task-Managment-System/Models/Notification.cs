a6e8ed9223db8343bfd73506b40c0adf92395b70
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2021-05-26 08:43:28 -0500

    Add Notification Helper class

--- Task-Managment-System/Task-Managment-System/Models/Notification.cs
+++ Task-Managment-System/Task-Managment-System/Models/Notification.cs
@@ -12,8 +12,19 @@ namespace Task_Managment_System.Models
         public string Title { get; set; }
         public string Contents { get; set; }
         public bool Read { get; set; }
-        public NotificationType NotificationType { get; set; }
+        public DateTime CreationDate { get; set; }
+        public NotificationType Type { get; set; }
 
         public virtual ApplicationUser User { get; set; }
+
+        public Notification(string userId, string title, string  contents, NotificationType type)
+        {
+            UserId = userId;
+            Title = title;
+            Contents = contents;
+            Read = false;
+            CreationDate = DateTime.Now;
+            Type = type;
+        }
     }
 }
---
+++ Task-Managment-System/Task-Managment-System/Models/NotificationHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Task_Managment_System.Models
+{
+    public class NotificationHelper
+    {
+        private static ApplicationDbContext db = new ApplicationDbContext();
+
+        public static void Create(string userId, string title, string contents, NotificationType type)
+        {
+            if (userId == null || title == null || contents == null)
+                return;
+            var user = db.Users.Find(userId);
+            if (user == null)
+                return;
+
+            var notification = new Notification(userId, title, contents, type);
+            db.Notifications.Add(notification);
+            db.SaveChanges();
+        }
+
+        public static void Delete(int id)
+        {
+            var notification = db.Notifications.Find(id);
+            if (notification == null)
+                return;
+
+            db.Notifications.Remove(notification);
+            db.SaveChanges();
+        }
+
+        public void Update(int notificationId)
+        {
+            var notification = db.Notifications.Find(notificationId);
+            if (notification == null)
+                return;
+            db.Entry(notification).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
